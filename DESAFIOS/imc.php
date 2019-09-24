a1721c6c549b0798ded94535f5330f28957f7238
Arthur Fernando <42222706+arthurff@users.noreply.github.com>
2019-09-24 20:21:53 -0300

    desafios-php

---
+++ DESAFIOS/imc.php
@@ -0,0 +1,38 @@
+<?php
+function calcula_obesidade($peso, $altura)
+{
+ return $peso / ($altura * $altura);
+}
+
+$massa = calcula_obesidade(90, 1.70);
+
+
+if ($massa <= 17) {
+$mensagem = "Muito abaixo do peso";
+}
+elseif(($massa > 17) and ($massa <=18.49)) {
+$mensagem = "Abaixo do peso";
+}
+elseif(($massa > 18.49) and ($massa <=24.99)) {
+$mensagem = "Peso Normal";
+}
+
+ 
+elseif(($massa > 24.99) and ($massa <=29.99)) {
+$mensagem = "Acima do Peso";
+}
+elseif(($massa > 29.99) and ($massa <=34.99)) {
+$mensagem = "Obesidade I";
+}
+elseif(($massa > 34.99) and ($massa <=39.99)) {
+$mensagem = "Obesidade II (severa)";
+}
+else { 
+$mensagem = "Obesidade III (mórbida)";
+}
+
+echo "Sua Massa Corporal é: <b> $massa </b><br>";
+echo "Estado Atual: <b> $mensagem</b>";
+
+
+?>
---
+++ DESAFIOS/tabuada.php
@@ -0,0 +1,8 @@
+<?php
+
+$contador = 10;
+
+while ($contador <= 100) {
+    echo "$contador" . "<BR>";
+    $contador = $contador + 10;
+}
